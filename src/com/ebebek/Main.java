fc0cc22fd12f55cf60bdf0a493983ac262f0a18c
Merve-Albayrak <mervealbayrak8@gmail.com>
2022-11-09 22:03:57 +0300

    vucut kitle indeksi

---
+++ src/com/ebebek/Main.java
@@ -0,0 +1,22 @@
+package com.ebebek;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner scanner=new Scanner(System.in);
+        System.out.print("lütfen boyunuzu giriniz:");
+        float boy=scanner.nextFloat();
+
+        System.out.print("lütfen kilonuzu giriniz:");
+        float kilo=scanner.nextFloat();
+        float indeks=kilo/(boy*boy);
+
+        System.out.println("Vücut Kitle İndeksiniz:"+indeks);
+
+
+
+    }
+}
